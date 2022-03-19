d15126a7ffbe3671b7b8b0175cd3e1517109ea04
Gavin Ong <kukudoom@gmail.com>
2022-03-19 23:10:51 +0800

    removed useless console.log

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -56,9 +56,6 @@ const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = ( await User.findOne({email: session.customer_email}))._id;
     const price = (session.amount_total / 100);
-
-    console.log(tour, user, price)
-
     await Booking.create({tour, user, price});
 }
 
